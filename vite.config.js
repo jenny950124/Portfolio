1b5fa9b84e0287b9a2f634795400c539703f49ff
Jenny Kim <nkim54@dtc01l0860-22.ad.bcit.ca>
2025-06-05 13:59:17 -0700

    🔧 Make footer responsive

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/', // ✅ 꼭 넣어야 함
+  base: '/', // ✅ 루트 도메인에서는 '/' 로 설정
   plugins: [react()],
   build: {
     outDir: 'dist',
